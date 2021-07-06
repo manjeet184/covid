80489aa55cbda64fc182aa8824740a060b23c9d7
Manjeet Bhardwaj <manjeet184jeet@gmail.com>
2021-07-06 17:30:02 +0530

    update

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from 'react';
+import { Container } from '@material-ui/core';
 
 const About = () => {
     return (
-        <div style={{ marginTop: 120 }}>
-            <h3>Hello from About sectiosn</h3>
-        </div>
+        <Container style={{ marginTop: 120 }}>
+            <h1>Vaccine Availability Checker</h1>
+            <h5>This project is developed in React environment. It shows the real time vaccine availabilty using the PIN code of the place. This can be very helpful to the people for the proper awarness of the resource. </h5>
+            <h6>Tech Used: Reactjs, react-hook, Material UI</h6>
+            <h6>Data Retrival: using cowin open api. </h6>
+            <h6>Data Entry: using Fetch API, stats and props.</h6>
+            <h6>Time Taken: 1 month</h6>
+            <h6>Team Member: 1(self)</h6>
+        </Container>
     )
 }
 
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,23 +6,42 @@ import Slot from './Slot';
 const Home = () => {
     const [data, setData] = useState([]);
     const [pin, setPin] = useState("000000");
-    let dat = "04-07-2021";
-
+    let [date, setdate] = useState("dd-mm-yyyy")
+    const [filt, setFilt] = useState([]);
 
     const getDetail = async () => {
-        await fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${dat}`).then((result) => {
+        await fetch(`https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${date}`).then((result) => {
             result.json().then((resp) => {
-                console.log(resp.sessions)
-                setData(resp.sessions)
+                // console.log(resp.sessions)
+                setData(resp.sessions);
+                setFilt(data);
             })
         })
-    }
-    const savePin = () => {
-        getDetail();
     }
     useEffect(() => {
         getDetail();
     }, [])
+    const getAvl = () => {
+        let fil = data.filter((e) => e.available_capacity !== 0)
+        setFilt(fil)
+    }
+
+
+    const getFree = () => {
+        let free = data.filter((e) => e.fee_type === "Free")
+        setFilt(free)
+    }
+    const getPaid = () => {
+        let paid = data.filter((e) => e.fee_type === "Paid")
+        setFilt(paid)
+    }
+
+    date = date.split("-").reverse().join("-");
+
+    const savePin = () => {
+        getDetail()
+    }
+
     // data.map((item) => console.log(item.name))
     return (
         <>
@@ -32,9 +51,12 @@ const Home = () => {
                         <TextField
                             label="Enter Pin Code"
                             variant="standard"
-                            // type=""
+                            type="number"
                             onChange={(e) => { setPin(e.target.value) }}
                         />
+                        <label style={{ marginInline: "50px" }}>
+                            Date : <input type="date" onChange={(e) => { setdate(e.target.value) }}></input>
+                        </label>
                         <Button
                             variant="contained"
                             color="primary"
@@ -44,9 +66,14 @@ const Home = () => {
                     </Box>
                 </Toolbar>
             </Container>
+            <Container style={{ flexGrow: 1, marginInline: 70, marginBottom: 25 }}>
+                <Button variant="outlined" color="primary" onClick={getAvl}>Only available</Button>
+                <Button style={{ marginInline: 15 }} variant="outlined" color="primary" onClick={getFree}>Free</Button>
+                <Button variant="outlined" color="primary" onClick={getPaid}>Paid</Button>
+            </Container>
             <Container>
                 <Grid container spacing={3}>
-                    {data.map((item) => (
+                    {filt.map((item) => (
                         <Grid item key={item.session_id} xs={12} sm={6} md={4} lg={3}>
                             <Slot item={item} />
                         </Grid>
